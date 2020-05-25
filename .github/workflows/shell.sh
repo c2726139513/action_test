f481346efe19d59671070896708f9eb5ef17b105
c2726139513 <c2726139513@outlook.com>
2020-05-25 11:51:08 +0800

    test

--- .github/workflows/shell.sh
+++ .github/workflows/shell.sh
@@ -4,6 +4,6 @@ for i in 1 2 3 4 5 6 7 8 9
 do 
         cp test.yml test$i.yml
 	sed -i "s/Test/Test$i/g" test$i.yml
-	sed -i "s/Cxl0%%/Cxl$i%%/g" test$i.yml
+	sed -i "s/cxl0%%/cxl$i%%/g" test$i.yml
 done
 
